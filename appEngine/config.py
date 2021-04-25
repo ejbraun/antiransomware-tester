e4ed2b0cabcfc4bbc07231c6c0390499845643eb
evan <evanbraun@berkeley.edu>
2021-04-25 10:27:03 -0600

    config change

--- appEngine/config.py
+++ appEngine/config.py
@@ -29,7 +29,7 @@ sub_name = ''
 # User defined name of a particular experiment
 experimentName = ''
 # Where the mock ransomware payload is directed to begin execution
-rootPairString = './testDirectory'
+rootPairString = '.\\files'
 # Maximum number of retries allowed for a test before marked as failed
 max_retries = 3
 # Logging object
